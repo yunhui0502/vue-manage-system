654b2ad76623591d5467e65853b28b99b4cbf2ec
Uaaa <3040568112@qq.com>
2019-10-20 13:28:50 +0800

    1

--- config/index.js
+++ config/index.js
@@ -24,8 +24,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    // host: '172.31.22.54', // can be overwritten by process.env.HOST
-    host: 'localhost',
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 3000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
