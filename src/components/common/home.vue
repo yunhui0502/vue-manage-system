2e17a0ef2de82e167800f660969963562cb21b20
davin <davinzhang@outlook.com>
2019-12-20 07:19:54 +0800

    kkk

--- src/components/common/home.vue
+++ src/components/common/home.vue
@@ -38,8 +38,8 @@
 <script>
 import vHead from '@/components/common/header.vue';
 import vSidebar from '@/components/common/sidebar.vue';
-import vTags from '@/components/common/tags.vue';
-import vFooter from '@/components/common/footer.vue';
+import vTags from '@/components/common/Tags.vue';
+import vFooter from '@/components/common/Footer.vue';
 import hanfuBus from '@/components/common/hanfu-bus';
 export default {
     data() {
