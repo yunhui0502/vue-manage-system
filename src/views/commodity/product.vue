9ce46ee131d73394e51597ab3d16a6dcfbedb246
yunhui <giteehui@163.com>
2019-12-20 20:53:31 +0800

    商品 核销 完成

--- src/views/commodity/product.vue
+++ src/views/commodity/product.vue
@@ -8,50 +8,48 @@
       <el-container>
         <el-form
           :model="ruleForm"
+          :rules="rules"
           ref="ruleForm"
           label-width="280px"
           class="demo-ruleForm"
         >
-          <el-form-item label="商品描述" prop="productDesc">
+          <el-form-item label="商品标题" prop="productDesc">
             <el-input style="width:100%" v-model="ruleForm.productDesc"></el-input>
           </el-form-item>
-          <el-form-item style="width:50%" label="商品名称" prop="hfName">
-            <el-input v-model="ruleForm.hfName"></el-input>
+          <el-form-item style="width:100%" label="商品名称" prop="hfName">
+            <el-input style="width:40%" v-model="ruleForm.hfName"></el-input>
+            <span style="margin: 22px">核销员</span>
+            <el-input v-model="verifier" style="width:40%"></el-input>
           </el-form-item>
-          <el-form-item style="width:50%" label="商品ID" prop="id">
-            <el-input  v-model="ruleForm.id"></el-input>
+          <el-form-item style="width:100%" label="总库存数" prop="inventory">
+            <el-input style="width:40%" v-model="ruleForm.inventory"></el-input>
+            <span>件</span>
+            <span style="margin: 10px">商品属性</span>
+            <el-input style="width:40%" v-model="ruleForm.brandId"></el-input>
           </el-form-item>
           <el-form-item label="商品类目" prop="categoryId">
             <el-select style="width:33.3%" v-model="ruleForm.categoryId" placeholder="上衣">
               <el-option
-                  v-for="(item, index) in leiMu"
-                  :key="index"
-                  :label="item.hfName"
-                  :value="item.hfName"
-                  style="height: 45px;"
+                v-for="(item, index) in leiMu"
+                :key="index"
+                :label="item.hfName"
+                :value="item.hfName"
+                style="height: 45px;"
               ></el-option>
             </el-select>
-            <el-select style="width:33.3%" v-model="ruleForm.region" placeholder="卫衣">
-
-            </el-select>
-            <el-select style="width:33.3%" v-model="ruleForm.region" placeholder="乐事联名">
-
-            </el-select>
+            <el-select style="width:33.3%" v-model="ruleForm.region" placeholder="卫衣"></el-select>
+            <el-select style="width:33.3%" v-model="ruleForm.region" placeholder="乐事联名"></el-select>
           </el-form-item>
-          <el-form-item label="品牌ID" prop="brandId">
+          <el-form-item label="添加至" prop="brandId">
             <el-input style="width:40%" v-model="ruleForm.id"></el-input>
 
-            <el-button
-              style="width:25%; margin-left: 5px;"
-              type="primary"
-              @click="submitForm"
-            >+新建商品</el-button>
+            <el-button style="width:25%; margin-left: 5px;" type="primary" @click="submitForm">+新建商品</el-button>
             <el-button style="width:25%" @click="resetForm('ruleForm')">批量添加</el-button>
           </el-form-item>
         </el-form>
       </el-container>
     </el-card>
-
+<!-- ---------------------------------------------------------------------------------------------------------------------------------------------------- -->
     <el-card class="box-card">
       <div slot="header" class="clearfix">
         <span>大卡片名称</span>
@@ -61,23 +59,31 @@
       <div class="kaipi">
         <el-button class="sc-delete" type="text">删除型号</el-button>
         <div>
-          <el-input style="width:304px;height: 32px; margin: 24px 0" placeholder="请输入内容"></el-input>
+          <el-input
+            v-model="input"
+            style="width:304px;height: 32px; margin: 24px 0"
+            placeholder="请输入内容"
+          ></el-input>
           <!-- 单选按钮 -->
           <el-checkbox v-model="checked">添加规格图片</el-checkbox>
         </div>
         <!-- 标签 -->
         <el-tag
           :key="tag"
-          v-for="tag in dynamicTags"
+          v-for="tag in specificationForm.specValue"
           closable
           :disable-transitions="false"
           @close="handleClose(tag)"
         >{{tag}}</el-tag>
         <!-- 图片上传图片 -->
         <el-upload
-          action="https://jsonplaceholder.typicode.com/posts/"
+          action="http://192.168.1.104:9095/goods/addPicture"
           list-type="picture-card"
           :on-preview="handlePictureCardPreview"
+          name="fileInfo"
+          file="file"
+          :on-error="success"
+          :data="{...specificationForm }"
           :on-remove="handleRemove"
         >
           <i class="el-icon-plus"></i>
@@ -85,32 +91,33 @@
         <el-dialog :visible.sync="dialogVisible">
           <img width="100%" :src="dialogImageUrl" alt />
         </el-dialog>
-        <el-input style="width:194px; height:32px;" placeholder="请输入内容"></el-input>
-        <el-button style="margin:8px">添加商品规格</el-button>
+        <el-input v-model="AddColor" style="width:194px; height:32px;" placeholder="请输入内容"></el-input>
+        <el-button   @click="submit" style="margin:8px">添加商品规格</el-button>
+        <el-input v-model="specificationForm1.productSpecId"  placeholder="添加商品id"></el-input>
       </div>
-      <el-button class="add-button">添加</el-button>
+      <el-button  @click="postspecification" class="add-button">添加</el-button>
 
-      <div class="model">
+      <!-- <div class="model">
         <el-button class="sc-delete" type="text">删除型号</el-button>
         <div>
-          <el-input style="width:304px;height: 32px; margin: 24px 0" placeholder="请输入内容"></el-input>
+          <el-input v-model="AddSize" style="width:304px;height: 32px; margin: 24px 0" placeholder="请输入内容"></el-input>
         </div>
-        <div>
+        <div> -->
           <!-- 标签 -->
-          <el-tag
+          <!-- <el-tag
             :key="tag"
             v-for="tag in modelTags"
             closable
             :disable-transitions="false"
             @close="handleClose(tag)"
-          >{{tag}}</el-tag>
+          >{{tag}}</el-tag> -->
           <!-- 单选按钮 -->
-          <el-checkbox v-model="checked2">添加规格图片</el-checkbox>
+          <!-- <el-checkbox v-model="checked2">添加规格图片</el-checkbox>
         </div>
         <el-input style="width:194px; height:32px;" placeholder="请输入型号"></el-input>
         <el-button style="margin:8px">添加规格项目</el-button>
       </div>
-      <el-button class="add-button">添加</el-button>
+      <el-button class="add-button">添加</el-button> -->
 
       <!-- 表单部分 商品价格/库存 -->
       <span style="position: relative;top: 57px;left:-830px">商品价格/库存</span>
@@ -159,16 +166,9 @@
         </el-table-column>
       </el-table>
       <!-- ------------------------------------------------------------------------------------------ -->
-      <el-form
-        :model="ruleForm2"
-        :rules="rules"
-        ref="ruleForm"
-        label-width="110px"
-        class="reduceForm"
-      >
+      <el-form :model="ruleForm2" ref="ruleForm" label-width="110px" class="reduceForm">
         <el-form-item label="库存减扣方式" prop="region">
-          <el-select v-model="ruleForm2.region" placeholder="拍下减库存">
-          </el-select>
+          <el-select v-model="ruleForm2.region" placeholder="拍下减库存"></el-select>
         </el-form-item>
 
         <el-form-item style="width:21%" label="库存" prop="name">
@@ -176,7 +176,7 @@
         </el-form-item>
 
         <el-form-item style="width:21%" label="商品详情" prop="name">
-          <el-upload
+          <!-- <el-upload
             action="https://jsonplaceholder.typicode.com/posts/"
             list-type="picture-card"
             :on-preview="handlePictureCardPreview"
@@ -186,7 +186,7 @@
           </el-upload>
           <el-dialog :visible.sync="dialogVisible">
             <img width="100%" :src="dialogImageUrl" alt />
-          </el-dialog>
+          </el-dialog> -->
         </el-form-item>
       </el-form>
       <el-button class="add-button">添加</el-button>
@@ -225,41 +225,11 @@
           <span class="liji">立即购买按钮</span>
           <el-radio v-model="radio" label="1">系统默认</el-radio>
           <el-radio v-model="radio" label="2">自定义</el-radio>
-          <el-input style="width: 10%;"  placeholder="6字之内"></el-input>
+          <el-input style="width: 10%;" placeholder="6字之内"></el-input>
         </div>
       </el-form>
     </el-card>
 
-    <el-card class="the-card">
-      <div slot="header" class="clearfix">
-        <span>更多设置</span>
-      </div>
-      <el-form>
-        <span style="position: relative;top: 16px;left:-100px;font-size: 18px;">预售</span>
-        <el-form-item label="">
-         <el-checkbox v-model="checked3">无现货，下单后需要过一段时间再发货</el-checkbox>
-        </el-form-item>
-
-         <el-form-item label="">
-           <span style="position: relative;top: 20px;left:-100px; font-size: 18px;">限购</span>
-           <div class="div-botton">
-              <el-checkbox v-model="checked3">限制每人购买数量</el-checkbox>
-           </div>
-           <div class="div-botton">
-              <el-checkbox v-model="checked3">终身限购</el-checkbox>
-              <el-input style="width: 10%;"  placeholder="6字之内"></el-input><span>件</span>
-           </div>
-           <div class="div-botton">
-              <el-checkbox v-model="checked3">终身限购</el-checkbox>
-            <el-select v-model="ruleForm.region" placeholder="拍下减库存">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-          <el-input style="width: 10%;" placeholder="6字之内"></el-input><span>件</span>
-           </div>
-        </el-form-item>
-      </el-form>
-    </el-card>
   </div>
 </template>
 
@@ -267,6 +237,13 @@
 export default {
   data () {
     return {
+      input: '',
+      // 添加规格
+      AddSize: '',
+      // 添加颜色
+      AddColor: '',
+      // 核销员绑定的值
+      verifier: '',
       // 图片上传控件里的
       dialogImageUrl: '',
       dialogVisible: false,
@@ -278,8 +255,7 @@ export default {
       // 多选择框
       checked3: true,
       // 标签
-      dynamicTags: ['黑色', '白色', '红色'],
-      modelTags: ['S码', 'M码'],
+      modelTags: [], // s码
       // 时间日期表单
       form: {
         date1: ''
@@ -287,6 +263,7 @@ export default {
       leiMu: {},
       // 表单绑定的添加商品
       ruleForm: {
+        inventory: '', // 库存
         bossId: '1', // 商家id
         brandId: '', // 品牌id
         categoryId: '', // 类目
@@ -294,10 +271,35 @@ export default {
         hfName: '', // 商品名称
         lastModifier: '1', // 商家昵称
         productDesc: '', // 产品描述 ''
-        requestId: '', // 发起请求的随机数, 用来判断请求是否重复
+        requestId: '111111111111', // 发起请求的随机数, 用来判断请求是否重复
         timestamp: '', // 当前时间
         token: '11238',
         userId: '12' // 用户id
+      },
+      // 添加图片
+      specificationForm: {
+        bossId: '1',
+        goodsId: '2', // 物品ID
+        productSpecId: '', // 商品规格ID
+        requestId: '123123123', // 发起请求的随机数, 用来判断请求是否重复
+        timestamp: '12231231', // 当前时间
+        token: '11238',
+        userId: '12' // 用户id
+        // specValue: ['1.1'] // 标签 颜色 // 规格
+      },
+      // 添加规格
+      specificationForm1: {
+        productId: '3',
+        bossId: '3',
+        goodsDesc: '1111', // 物品描述
+        goodName: '3', // 物品名称
+        hfStoreId: '3', // 商铺id
+        requestId: '123123123', // 发起请求的随机数, 用来判断请求是否重复
+        timestamp: '12231231', // 当前时间
+        token: '11238',
+        userId: '12', // 用户id
+        username: '1'// 商家名称
+        // specValue: ['1.1'] // 标签 颜色 // 规格
       },
       ruleForm2: {
         name: '',
@@ -311,53 +313,77 @@ export default {
       },
       // 表单数据
       tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎'
-        },
-        {
-          date: '2016-05-04',
-          name: '王小虎'
-        },
-        {
-          date: '2016-05-01',
-          name: '王小虎'
-        },
-        {
-          date: '2016-05-03',
-          name: '王小虎'
-        }
+
       ],
       // 顶部表单验证
       rules: {
-        name: [
+        hfName: [
           { required: true, message: '请输入活动名称', trigger: 'blur' },
           { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-        ],
-        region: [
-          { required: true, message: '请选择活动区域', trigger: 'change' }
         ]
       }
     }
   },
   created () {
     this.getcategory()
+    this.getcoommo()
   },
   methods: {
-    // 点击图片列表中已上传的图片时的钩子
+    // 获取商品列表
+    async getcoommo () {
+      const {
+        data: { data }
+      } = await this.$http.get('http://192.168.1.104:9095/goods/categoryId', { params: { bossId: 1 } })
+      this.tableData = data
+    },
+    // 添加图片
+
+    // 添加规格
+    async postspecification () {
+      this.time()
+      this.specificationForm.requestId = Date.now()
+      this.$http.post('http://192.168.1.104:9095/goods/addSpecify', { params: this.specificationForm1 })
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      // console.log(this.dialogImageUrl)
+      // if (this.radio === 1) {
+      //   await this.$http.post('http://192.168.1.104:9095/goods/addPicture', { fileInfo: this.dialogImageUrl, ...this.specificationForm })
+      //     .then(res => {
+      //       console.log(res)
+      //     })
+      //     .catch(err => {
+      //       console.log(err)
+      //     })
+      // }
+      // await this.$http.post('http://192.168.1.104:9095/goods/addSpecify', { params: { ...this.specificationForm } })
+    },
+    // 添加颜色事件
+    submit (e) {
+      // console.log(this.AddColor)
+      this.specValue = e.target.value
+      this.AddColor = ''
+    },
+    // 图片列表移除图片时的钩子
     handleRemove (file, fileList) {
       console.log(file, fileList)
     },
-
-    // 图片列表移除图片时的钩子
+    success (err, file, fileList) {
+      console.log(err)
+    },
+    // 点击图片列表中已上传的图片时的钩子
     handlePictureCardPreview (file) {
+      console.log(1111111)
       this.dialogImageUrl = file.url
       this.dialogVisible = true
     },
 
     // 标签点击按钮
     handleClose (tag) {
-      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
+      this.specificationForm.specValue.splice(this.specificationForm.specValue.indexOf(tag), 1)
     },
     // 获取类目
     async getcategory () {
@@ -367,6 +393,7 @@ export default {
       this.leiMu = data
       // this.leimu.levelId = data.length
     },
+
     // 添加商品
     async postcoommo () {
       await this.$http.post('http://192.168.1.104:9095/product/addproduct', {
@@ -383,12 +410,11 @@ export default {
     },
     // 新建商品按钮
     submitForm: function () {
-      console.log('.....------')
+      this.time()
       this.$refs.ruleForm.validate(valid => {
         console.log(valid)
         if (valid) {
           this.$confirm('确认提交吗？', '提示', {}).then(() => {
-            this.time()
             this.postcoommo()
             this.addLoading = false
             this.$message({
@@ -399,6 +425,9 @@ export default {
           })
         }
       })
+    },
+    conver: function (s) {
+      return s < 10 ? '0' + s : s
     },
     // 当前时间
     time () {
@@ -415,28 +444,23 @@ export default {
       var m = myDate.getMinutes() // 获取当前分钟数(0-59)
       var s = myDate.getSeconds()
       // 获取当前时间
-      var now =
-        year +
-        '-' +
-        this.conver(month) +
-        '-' +
-        this.conver(date) +
-        ' ' +
-        this.conver(h) +
-        ':' +
-        this.conver(m) +
-        ':' +
-        this.conver(s)
-      this.addForm.timestamp = now
-      this.addForm.requestId = now + 100
+
+      var now = year + '-' + month + '-' + date + ' ' + h + ':' + m + ':' + s
+      this.ruleForm.timestamp = now
+      // 发起请求的随机数, 用来判断请求是否重复
+      this.ruleForm.requestId = Date.now()
       // console.log(this.timestamp)
+
+      // specificationForm里的随机数和时间
+      this.specificationForm.timestamp = now
     },
     // 添加批量按钮
     resetForm (formName) {
       //  清空form表单
       this.$refs[formName].resetFields()
     }
-  }
+  },
+  mounted () {}
 }
 </script>
 
@@ -461,7 +485,7 @@ export default {
 }
 .kaipi {
   width: 676px;
-  height: 330px;
+  height: 100%;
   margin-left: 300px;
   border: 1px solid rgba(46, 45, 45, 0.09);
   padding-left: 30px;
---
+++ src/views/verification/index.vue
@@ -0,0 +1,306 @@
+<template>
+  <div>
+    <!-- 搜索 -->
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>搜索</span>
+      </div>
+      <div class="grid-content bg-purple" style="text-align:center">
+        <!-- 时间 -->
+        <span class="demonstration" style="margin-right:20px">时间查询</span>
+        <el-date-picker
+          v-model="value1"
+          type="daterange"
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+
+        ></el-date-picker>
+        <!-- 地区 -->
+        <span class="place">请选择地区</span>
+        <el-input v-model="input" placeholder="请输入内容" style="width:150px"></el-input>
+      </div>
+      <!-- 按钮 -->
+      <el-row class="btn">
+        <el-button size="medium" style="background-color:#a6a3fb; color:#fff" @click="ppl" >查询</el-button>
+        <el-button size="medium" >重置</el-button>
+      </el-row>
+    </el-card>
+
+    <!-- 内容 -->
+    <el-card class="box-card" style="margin-top: 20px">
+      <div slot="header" class="clearfix">
+        <el-row class="btn">
+          <!-- 删除按钮 -->
+          <el-button size="medium" @click="shanchu">批量删除</el-button>
+        </el-row>
+        <!-- 点击弹框添加 -->
+        <el-dialog title="添加核销员" :visible.sync="dialogFormVisible">
+          <el-form :model="form">
+            <el-form-item label="核销员名称" :label-width="formLabelWidth">
+              <el-input
+                v-model="form.id"
+                autocomplete="off"
+                placeholder="请输入核销员名称"
+                style="width:300px"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="核 销 地 点" :label-width="formLabelWidth">
+              <el-input
+                v-model="form.name1"
+                autocomplete="off"
+                placeholder="请输入核销地点"
+                style="width:300px"
+              ></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer" style="text-align: center;">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button
+              type="primary"
+              @click="dialogFormVisible = false"
+              style="margin-left: 50px;background-color:#a6a3fb; color:#fff;border:0"
+            >保 存</el-button>
+          </div>
+        </el-dialog>
+        <!-- /点击弹框添加 -->
+      </div>
+
+      <!-- 列表 -->
+      <el-checkbox v-model="checked">已选4人</el-checkbox>
+      <span style="color:#ff2e88; margin-left:20px">清空</span>
+      <el-table ref="multipleTable" :data="see" tooltip-effect="dark" style="width: 100%">
+        <el-table-column type="selection" width="55"></el-table-column>
+        <el-table-column prop="realName" label="核销员" width="120"></el-table-column>
+        <el-table-column prop="money" label="核销额" width="120"></el-table-column>
+        <el-table-column prop="presentMoney" label="本次核销额" width="120"></el-table-column>
+        <el-table-column prop="site" label="地址" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="createDate" label="创建时间" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="address" label="操作" show-overflow-tooltip>
+          <el-button type="text" style="color:#a6a3fb" @click="compile(see)" this.data >编辑</el-button>
+          <el-button type="text" style="color:#fad177" @click="open = true">记录</el-button>
+          <el-button type="text" style="color:#ff2e88">清除记录</el-button>
+        </el-table-column>
+      </el-table>
+      <!-- --------------------------------------------------------------- -->
+      <el-dialog title="编辑核销员" :visible.sync="dialogFormVisible">
+        <el-form :model="seeform" >
+          <el-form-item label="核 销 物 品" :label-width="formLabelWidth">
+            <el-select v-model="seeform" placeholder="请选择">
+            <el-option
+              v-for="(item,i) in options"
+              :key="i"
+              :label="item.label"
+            ></el-option>
+          </el-select>
+          </el-form-item>
+          <el-form-item label="核销员名称" :label-width="formLabelWidth" >
+            <el-input
+              v-model="seeform.RealName"
+              autocomplete="off"
+              placeholder="请输入核销员名称"
+              style="width:300px"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="核 销 地 点" :label-width="formLabelWidth">
+            <el-input
+              v-model="seeform.site"
+              autocomplete="off"
+              placeholder="请输入核销地点"
+              style="width:300px"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer" style="text-align: center;">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button
+            type="primary"
+            @click="save"
+            style="margin-left: 50px;background-color:#a6a3fb; color:#fff;border:0"
+          >保 存</el-button>
+        </div>
+      </el-dialog>
+      <!-- --------------------------------------------------------------- -->
+      <!-- 点击弹框添加 -->
+      <el-dialog title="核销记录" :visible.sync="open">
+        <el-table :data="rec" style="width: 100%">
+          <el-table-column prop="purchasePrice" label="核销金额" width="180"></el-table-column>
+          <el-table-column prop="createDate" label="核销时间" width="180"></el-table-column>
+          <el-table-column prop="purchaseQuantity" label="核销金额" width="180"></el-table-column>
+          <el-table-column prop="createDate" label="核销时间" width="180"></el-table-column>
+        </el-table>
+        <div style="margin-top:10px;text-align: right; ">
+          <el-pagination background layout="prev, pager, next, jumper" :total="1000"></el-pagination>
+        </div>
+      </el-dialog>
+      <!-- /点击弹框添加 -->
+      <div class="sum">合计：5000.00元</div>
+      <!-- 分页 -->
+      <div style="margin-top:10px;text-align: right; ">
+        <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data () {
+    return {
+      rec: '',
+      // 列表上的双绑定
+      checked: '',
+      value1: '',
+      input: '',
+      dialogFormVisible: false,
+      open: false,
+      see: [],
+      tian: [],
+      options: [
+
+      ],
+      seeform: {
+        RealName: '',
+        site: '',
+        UserId: '11',
+        cancel2: '1'
+      },
+      shan: [],
+      form: {
+        name: '',
+        name1: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      formLabelWidth: '200px'
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {
+    this.seek()
+    this.record()
+    // this.addition()
+    this.area()
+    this.ppl()
+    this.time()
+    this.tianjia()
+    this.shanchu()
+  },
+  methods: {
+    async save () {
+      this.compile()
+      await this.$http.get(
+        'http://192.168.1.125:9901/cancel/updateCancel', { params: this.seeform }
+      ).then(function (res) {
+        // handle success
+        // console.log(this.seeform)
+
+        this.dialogFormVisible = false
+      })
+        .catch(function (err) {
+          // handle error
+          console.log(err)
+        })
+    },
+    async seek () {
+      const data = await this.$http.get(
+        'http://192.168.1.125:9901/cancel/selectCancel'
+      )
+      // console.log(data.data.data)
+      this.see = data.data.data
+    },
+    compile (see) {
+      this.dialogFormVisible = true
+      // console.log(see)
+      // var index = '',
+      for (let i = 0; i <= see.length - 1; i++) {
+        console.log(i)
+        this.seeform.RealName = see[i].realName
+        this.seeform.site = see[i].site
+      }
+    },
+    async record () {
+      const data = await this.$http.get(
+        'http://192.168.1.125:9901/cancel/selectCancelId'
+      )
+      console.log(data.data.data)
+      this.rec = data.data.data
+    },
+    // async addition () {
+    //   const data = await this.$http.get(
+    //     'http://192.168.1.125:9901/cancel/updateCancel'
+    //   )
+    //   console.log(data.data.data)
+    //   this.form = data.data.data
+    // },
+    async area () {
+      const data = await this.$http.get(
+        'http://192.168.1.125:9901/cancel/selectRegion'
+      )
+      console.log(data.data.data)
+      this.are = data.data.data
+    },
+    async time () {
+      const data = await this.$http.get(
+        'http://192.168.1.125:9901/cancel/selectDate'
+      )
+
+      console.log(data.data.data)
+      this.ti = data.data.data
+    },
+    async ppl () {
+      const data = await this.$http.get(
+        'http://192.168.1.125:9901/test/login'
+      )
+
+      console.log(data.data.data)
+      this.pp = data.data.data
+    },
+    async tianjia () {
+      const data = await this.$http.get(
+        'http://192.168.1.125:9901/cancel/updateCancelUser'
+      )
+
+      console.log(data.data)
+      this.tian = data.data
+    },
+    async shanchu () {
+      const data = await this.$http.get(
+        'http://192.168.1.125:9901/cancel/deleteCancel'
+      )
+
+      console.log(data.data.data)
+      this.shan = data.data.data
+    }
+
+  },
+
+  mounted () {}
+}
+
+</script>
+
+<style scoped lang="less">
+.place {
+  margin-left: 50px;
+  margin-right: 20px;
+}
+.btn {
+  text-align: right;
+  margin: 20px 30px 0 0;
+}
+.sum {
+  margin-top: 10px;
+  margin-left: 150px;
+  color: #ff2e88;
+}
+</style>
