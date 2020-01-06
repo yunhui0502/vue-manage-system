8ab25ee210a5d8980d89c0122df6d10bc46f8dec
davin <davinzhang@outlook.com>
2020-01-07 07:11:15 +0800

    fixed

--- vue.config.js
+++ vue.config.js
@@ -6,18 +6,14 @@ module.exports = {
       process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/',
   // 当运行 vue-cli-service build 时生成的生产环境构建文件的目录。
   // 注意目标目录在构建之前会被清除 (构建时传入 --no-clean 可关闭该行为)。
-  // 默认值'dist'
-  outputDir: 'my-app',
   // 放置生成的静态资源 (js、css、img、fonts) 的目录(相对于outputDir目录)。
   // 默认值''
   assetsDir: 'assets',
   // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
-  // 默认值'index.html'
-  indexPath: 'myIndex.html',
   // 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。
-  filenameHashing: false,
+  filenameHashing: true,
   // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码。这个值会在 @vue/cli-plugin-eslint 被安装之后生效。
-  lintOnSave: process.env.NODE_ENV !== 'production',
+  lintOnSave: true,
 
   // 是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。
   runtimeCompiler: false,
@@ -27,7 +23,6 @@ module.exports = {
 
   // 所有 webpack-dev-server 的选项都支持。
   devServer: {
-    host: '172.26.81.247',
     port: 3001, // 端口号
     https: false,
     open: true, // 配置自动启动浏览器
