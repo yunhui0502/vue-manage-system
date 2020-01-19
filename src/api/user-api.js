deb6db75ce59e5f5848f51757d682a3fc8801f0b
yunhui <giteehui@163.com>
2020-01-19 19:42:04 +0800

    用户 添加核销员 修改用户信息 完成

--- src/api/user-api.js
+++ src/api/user-api.js
@@ -4,7 +4,36 @@ import Axios from 'axios'
 function categoryId () {
   return Axios.get('/log/user/selectList')
 }
+// 添加核销员
+function insertCancel (params) {
+  return Axios.get('/ver/cancel/insertCancel?RealName=' + params.RealName + '&UserId=' + params.UserId + '&site=' + params.site)
+}
+// 编辑
+function update (params) {
+  let fd = new FormData()
+  fd.append('userStatus', params.userStatus)
+  fd.append('realName', params.realName)
+  // fd.append('birthDay', params.birthDay)
+  fd.append('phone', params.phone)
+  fd.append('requestId', params.requestId)
+  fd.append('sex', params.sex)
+  fd.append('token', params.token)
+  fd.append('userId', params.userId)
+  return Axios.post('/log/user/update', fd)
+}
 
+// 修改核销员信息
+function updateCancel (params) {
+  let fd = new FormData()
+  fd.append('RealName', params.realName)
+  fd.append('userId', params.userId)
+  fd.append('cancel2', params.cancel2)
+  fd.append('site', params.site)
+  return Axios.post('/ver/cancel/updateCancel', fd)
+}
 export default {
-  categoryId: categoryId
+  categoryId: categoryId,
+  insertCancel: insertCancel,
+  update: update,
+  updateCancel: updateCancel
 }
