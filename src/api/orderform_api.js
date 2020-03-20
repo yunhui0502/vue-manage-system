c5b69fb14b63cafccfea59588a6092574032fab7
Uaaa <3040568112@qq.com>
2020-03-21 07:06:50 +0800

    commit

--- src/api/orderform_api.js
+++ src/api/orderform_api.js
@@ -1,10 +1,12 @@
-
-import Axios from 'axios'
+// eslint-disable-next-line require-jsdoc
+import Axios from 'axios';
 // 查看订单状态
+// eslint-disable-next-line require-jsdoc
 function getProductList () {
-  return Axios.get('/foo//order/query')
+  return Axios.get('/foo//order/query');
 }
 // 活动 秒杀 编辑商品
+// eslint-disable-next-line require-jsdoc
 function update (params) {
   let fd = new FormData()
   fd.append('goodsId', params.goodsId)
@@ -13,7 +15,7 @@ function update (params) {
   fd.append('repertory', params.repertory)
   fd.append('startTime', params.startTime)
   fd.append('stopTime', params.stopTime)
-  return Axios.post('/jietu/kill/update', fd, { responseType: 'arraybuffer' })
+  return Axios.post('/jietu/kill/update', fd, { responseType: 'arraybuffer' });
 }
 // 活动 拼团 编辑商品
 function groupupdate (params) {
